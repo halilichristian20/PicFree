5c952b3236351af9cbefc35c5767d9885ffea71d
unknown <christian.halili20@gmail.com>
2023-06-07 15:25:48 +0800

    .

--- js/collections.js
+++ js/collections.js
@@ -13,6 +13,29 @@ async function fetchCollection() {
   return data;
 }
 
+// ########## Back-to-top
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // Random Background
 
 const API_KEY = "F_AlrteKQ31bG2Gz9qt6eP94IkHZdOjnYQafFuuKFGc";
--- js/videos.js
+++ js/videos.js
@@ -152,6 +152,29 @@ function showModal() {
   document.body.style.overflow = "hidden";
 }
 
+// ########## Back-to-top
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // ################### Display Random Background from Unplash
 
 async function fetchAPIRandom(endpoint) {
